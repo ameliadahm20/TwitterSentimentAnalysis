3eaed0373ab8e333db44afca4b0d3fec9bc49407
Amelia Dahm <dahma@iu.edu>
2020-12-01 13:09:18 -0500

    added sentiment lib function

---
+++ py_files/sentiment_lib.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+
+def get_sentiment(df):
+    ###### Valence Shifters ######
+    v_shift = pd.read_csv('data/valenceshifter.csv')
+    
+    ###### NRC ######
+    nrc = pd.read_csv('data/NRC.csv')
+    nrc = pd.concat([nrc,v_shift])
+    sentiment = []
+    for tweet in df['lemmatizer_tweets']:
+        tweet = tweet.split(' ')
+        total = 0
+        count = 0
+        for word in tweet:
+            if word in list(nrc['word']):
+                total = total + (nrc[nrc['word'] == word].polar).iloc[0]
+                count = count + 1
+        try:
+            sent = (float(total) / float(count))
+        except:
+            sent = 0
+        sentiment.append(sent)
+    df['nrc_sentiment'] = sentiment
+    
+    
+
+    return df
