27d11c9d62c89c113d0a9c25ad67b453dd8cedf5
Amelia Dahm <dahma@iu.edu>
2020-12-01 13:12:05 -0500

    added gi lib

--- py_files/sentiment_lib.py
+++ py_files/sentiment_lib.py
@@ -24,6 +24,25 @@ def get_sentiment(df):
         sentiment.append(sent)
     df['nrc_sentiment'] = sentiment
     
+    ###### GI ######
+    gi = pd.read_csv('data/GI_en.csv')
+    gi = pd.concat([gi,v_shift])
+    sentiment = []
+    for tweet in df['lemmatizer_tweets']:
+        tweet = tweet.split(' ')
+        total = 0
+        count = 0
+        for word in tweet:
+            if word in list(gi['word']):
+                total = total + (gi[gi['word'] == word].polar).iloc[0]
+                count = count + 1
+        try:
+            sent = (float(total) / float(count))
+        except:
+            sent = 0
+        sentiment.append(sent)
+    df['gi_sentiment'] = sentiment
+    
     
 
     return df
